db623e725cc8e2c1cd414645f882f1990dbd7b8d
FaGalex <86808750+FaGalex@users.noreply.github.com>
2021-12-06 20:37:18 -0500

    Actualizacion proyecto

--- src/context/Api.js
+++
@@ -1,9 +0,0 @@
-// const server = "http://localhost:3000";
-const server = "https://app-campo-back.herokuapp.com";
-
-const apiRegister = `${server}/user`;
-const apiLogin = `${apiRegister}/auth`;
-const apiProduct = `${server}/product`;
-const apiProductuser = `${apiProduct}/user`
-
-export { apiRegister, apiLogin, apiProduct, apiProductuser };
