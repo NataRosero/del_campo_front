bc32eeae8c3fe6f37a82ff9bab5a34ed2fcc1115
FaGalex <86808750+FaGalex@users.noreply.github.com>
2021-12-06 20:31:58 -0500

    actualizacion link backend heroku

--- src/context/Api.js
+++ src/context/Api.js
@@ -1,7 +1,9 @@
-const server = "http://localhost:3000";
+// const server = "http://localhost:3000";
+const server = "https://app-campo-back.herokuapp.com";
+
 const apiRegister = `${server}/user`;
 const apiLogin = `${apiRegister}/auth`;
 const apiProduct = `${server}/product`;
 const apiProductuser = `${apiProduct}/user`
 
-export {apiRegister, apiLogin, apiProduct, apiProductuser};
+export { apiRegister, apiLogin, apiProduct, apiProductuser };
